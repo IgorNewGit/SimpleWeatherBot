a94f1566d87369d5073bbbf3220f2ef4a31efa72
Igor <igor.newgit@gmail.com>
2019-01-06 00:12:23 +0300

    Now the bot can resend the same text that a user typed into the bot

--- bot/src/com/igornewgit/bot/WeatherBot.java
+++ bot/src/com/igornewgit/bot/WeatherBot.java
@@ -1,14 +1,22 @@
 package com.igornewgit.bot;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class WeatherBot extends TelegramLongPollingBot {
 
-
     @Override
     public void onUpdateReceived(Update update) {
-
+        SendMessage message = new SendMessage();
+        message.setText(update.getMessage().getText());
+        message.setChatId(update.getMessage().getChatId());
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
